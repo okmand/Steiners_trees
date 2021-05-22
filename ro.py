c71e198f2da7fbddda0e53cab849ba8fd9fcda26
anok0420 <andrei.okmianskii@netcracker.com>
2021-05-22 16:58:43 +0300

    added colors to graph added result path and result length. but the path is being searched for incorrectly class ro: added edges

--- ro.py
+++ ro.py
@@ -1,9 +1,10 @@
 class Ro:
 
-    def __init__(self, vertex, weight):
+    def __init__(self, vertex, edges, weight):
         self.vertex = vertex
+        self.edges = edges
         self.weight = weight
 
     def __repr__(self):
-        return f"vertex -> {str(self.vertex)}; weight -> {str(self.weight)}"
+        return f"vertex -> {str(self.vertex)}, edges -> {str(self.edges)}, weight -> {str(self.weight)}"
 
--- stein.py
+++ stein.py
@@ -5,7 +5,6 @@ from itertools import combinations
 import collections
 from ro import Ro
 
-count_operation = 0
 max_weight = 100000
 
 
@@ -21,7 +20,7 @@ def initialize_graph(G):
     G.add_node(8, pos=(10, 10))
     G.add_node(9, pos=(30, 10))
     G.add_node(10, pos=(50, 10))
-    # G.add_node(11, pos=(70, 10))  # comment
+    G.add_node(11, pos=(70, 10))  # comment
     # G.add_node(12, pos=(95, 20))  # comment
     e = [(1, 2, 1), (1, 5, 4), (2, 5, 2),
          (2, 3, 1),
@@ -30,44 +29,41 @@ def initialize_graph(G):
          # (6, 7, 4), (7, 12, 4),  # comment
          (5, 8, 1), (8, 9, 3), (5, 9, 2),
          (9, 10, 1), (5, 6, 4), (6, 10, 4),
-         # (10, 11, 2), (6, 11, 2), (11, 12, 3)  # comment
+         (10, 11, 1), (6, 11, 2),
+         # (11, 12, 3)  # comment
          ]
     G.add_weighted_edges_from(e)
 
 
-def print_graph(G):
+def print_graph(G, result_path):
     pos = nx.get_node_attributes(G, 'pos')
     labels = nx.get_edge_attributes(G, 'weight')
     nx.draw(G, pos, with_labels=True)
-    # nx.draw_networkx_edges(G, pos, edgelist=[(5, 6)], edge_color="#4aff4a", )
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='red')
+    nx.draw_networkx_edges(G, pos, edge_color="#dedede", )
+    nx.draw_networkx_edges(G, pos, edgelist=result_path, edge_color="#f70909", )
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_color='#015e98')
     plt.show()
 
 
 # дейкстра для двух точек
 def dijkstra2(G, a, b):
-    global count_operation
-    count_operation += 1
     print("min path:", nx.dijkstra_path(G, a, b))
     print("min path length:", nx.dijkstra_path_length(G, a, b))
 
 
 # дейкстра для 3 точек
 def dijkstra3(G, init_nodes):
-    global count_operation
     min = 10000
     array_min_i = []
     for i in G.nodes:
         local_min = 0
 
         for init_node in init_nodes:
-            count_operation += 1
             local_min += nx.dijkstra_path_length(G, i, init_node)
 
         if local_min <= min:
             if local_min < min:
                 array_min_i = []
-            count_operation += 1
             min = local_min
             array_min_i.append(i)
 
@@ -109,29 +105,45 @@ def get_disjoint_subsets_function_body(arr, k, accum, index):
     return result
 
 
+# input: [1, 2, 5, 9, 10], output: [(1, 2), (2, 5), (5, 9), (9, 10)]
+def get_path_by_nodes(nodes_in_path):
+    true_path = []
+    for i in range(1, len(nodes_in_path)):
+        true_path.append(tuple([nodes_in_path[i - 1], nodes_in_path[i]]))
+
+    true_path = [tuple(sorted(path)) for path in true_path]  # [(2, 1), (5, 2), (5, 9)] -> [(1, 2), (2, 5), (5, 9)]
+    return true_path
+
+
+def sort_dict_by_keys(main_dict, keys):
+    result = {}
+    for key in keys:
+        result[key] = main_dict.get(key)
+    return result
+
+
 # дейкстра для более, чем 3 точек
 def dijkstra4(G, init_nodes):
     global check_print_debug
     if check_print_debug: print("init_nodes:", init_nodes)
-    global count_operation
     global max_weight
-    count_operation = 0
 
     all_coordinates = {}  # здесь будут храниться все координаты
-    init_nodes_with_out_last = init_nodes[:len(init_nodes) - 1]  # [1, 3, 8]
+    all_paths = {}  # здесь будут храниться все пути
+    init_nodes_with_out_last = init_nodes[:len(init_nodes) - 1]
     if check_print_debug: print("init_nodes_with_out_last", init_nodes_with_out_last)
 
     # 1 этап - 1-координаты
     for node in G.nodes:
-        path_length = {}
+        path = {}
         first_coordinate = {}
 
         for init_node in init_nodes_with_out_last:
-            count_operation += 1
             first_coordinate[(init_node,)] = nx.dijkstra_path_length(G, node, init_node)  # храним координаты в tuple
-            path_length[init_node] = nx.dijkstra_path_length(G, node, init_node)
+            path[(init_node,)] = get_path_by_nodes(nx.dijkstra_path(G, node, init_node))
 
         all_coordinates[node] = first_coordinate
+        all_paths[node] = path
 
     # ключ - вершина, значение - длина пути
     if check_print_debug:
@@ -147,6 +159,7 @@ def dijkstra4(G, init_nodes):
         for lot_omega in lots_of_omegas:
             nodes_to_subsets_omega = [lot_omega[0]]
             quasi_coordinates = {}
+            quasi_paths = {}
 
             omegas = []
             for i in range(1, len(lot_omega)):
@@ -162,28 +175,35 @@ def dijkstra4(G, init_nodes):
             for node in G.nodes:
                 if nodes_in_omega.count(node) == 0:  # перебираем все вершины кроме тех, что попали в омегу
                     quasi_coordinates[node] = max_weight
+                    quasi_paths[node] = []
 
             for omega in omegas:
                 if check_print_debug: print("\nOmega", omega)
 
-                if nodes_in_omega == [1, 10, 6]:
-                    udal = True
-
                 for node in quasi_coordinates:
                     current_coordinates = all_coordinates[node]
-                    quasi_coordinates[node] = min(quasi_coordinates[node],
-                                                  current_coordinates[tuple(sorted(omega[0]))] +
-                                                  current_coordinates[
-                                                      tuple(sorted(omega[1]))])
+                    current_paths = all_paths[node]
+                    sum_current_coordinates = current_coordinates[tuple(sorted(omega[0]))] + current_coordinates[
+                        tuple(sorted(omega[1]))]
+
+                    if quasi_coordinates[node] > sum_current_coordinates:
+                        quasi_coordinates[node] = sum_current_coordinates
+                        quasi_paths[node] = list(
+                            set(current_paths[tuple(sorted(omega[0]))] + current_paths[tuple(sorted(omega[1]))]))
 
             # упорядочиваем квазикоординаты в порядке возрастания
             quasi_coordinates = dict(
                 collections.OrderedDict(sorted(quasi_coordinates.items(), key=lambda kv: kv[1])))
+            # упорядочиваем пути по квазикоординатам
+            quasi_paths = sort_dict_by_keys(quasi_paths, quasi_coordinates.keys())
 
             if check_print_debug:
                 print("Quasi coordinates:\t", quasi_coordinates)
                 for key in quasi_coordinates:
                     print(key, ": ", quasi_coordinates[key], sep="")
+                print("Quasi paths:", )
+                for key in quasi_paths:
+                    print(key, ": ", quasi_paths[key], sep="")
 
             # вторая стадия
             fi = []
@@ -191,22 +211,32 @@ def dijkstra4(G, init_nodes):
             number_ro = 1
             lot_of_ro = {}
 
-            if check_print_debug: print("\n1 шаг")
             if check_print_debug: print(nodes_in_omega)
 
             # 1 шаг
             coordinate = sorted([node for node in nodes_in_omega if node != nodes_in_omega[0]])
-            weight = all_coordinates[nodes_in_omega[0]][tuple(coordinate)]
-            fi.extend(nodes_in_omega + [quasi for quasi in quasi_coordinates if quasi_coordinates[quasi] == weight])
-            pometka_lambda = {node: max_weight for node in G.adj if node not in fi}
+            first_weight = all_coordinates[nodes_in_omega[0]][tuple(coordinate)]
+            fi.extend(
+                nodes_in_omega + [quasi for quasi in quasi_coordinates if quasi_coordinates[quasi] == first_weight])
+            first_edges = all_paths[nodes_in_omega[0]][tuple(coordinate)]
+            for vertex in fi:
+                all_paths[vertex][tuple(sorted(nodes_in_omega))] = first_edges
 
-            lot_of_ro[number_ro] = Ro(fi.copy(), weight)
+            for key, edge in quasi_paths.items():
+                if key in fi:
+                    for e in edge:
+                        if e not in first_edges:
+                            first_edges.append(e)
+            pometka_lambda = {node: max_weight for node in G.adj if node not in fi}
+            pometka_lambda_path = {node: [] for node in G.adj if node not in fi}
+
+            lot_of_ro[number_ro] = Ro(fi.copy(), first_edges, first_weight)
             lot_of_pi[number_ro] = fi.copy()
 
-            quasi_coordinates = {key: value for key, value in quasi_coordinates.items() if value != weight}
+            quasi_coordinates = {key: value for key, value in quasi_coordinates.items() if value != first_weight}
+            quasi_paths = {key: value for key, value in quasi_paths.items() if key in quasi_coordinates}
 
             # следующие шаги
-            if check_print_debug: print("\nследующие шаги")
             all_nodes = [node for node in G.nodes if node not in fi]
 
             while len(all_nodes) > 0:
@@ -219,17 +249,38 @@ def dijkstra4(G, init_nodes):
                             if G.adj[node].get(vertex) is not None:
                                 new_min_weight = min(new_min_weight,
                                                      ro.weight + G.adj[node].get(vertex).get("weight"))
-                                pometka_lambda[node] = min(pometka_lambda[node],
-                                                           ro.weight + G.adj[node].get(vertex).get("weight"))
+                                if pometka_lambda[node] > ro.weight + G.adj[node].get(vertex).get("weight"):
+                                    pometka_lambda[node] = ro.weight + G.adj[node].get(vertex).get("weight")
+                                    pometka_lambda_path[node] = ro.edges + get_path_by_nodes(
+                                        # todo скорее всего неправильно
+                                        nx.dijkstra_path(G, node, vertex))
                 new_min_weight = min(new_min_weight, quasi_coordinates[list(quasi_coordinates.keys())[0]])
-                new_vertex = list(
-                    set([quasi for quasi in quasi_coordinates if quasi_coordinates[quasi] == new_min_weight] \
-                        + [node for node in pometka_lambda if pometka_lambda[node] == new_min_weight]))
+                new_vertex = []
+                all_new_edges = []
+                for coordinate in quasi_coordinates:
+                    if quasi_coordinates[coordinate] == new_min_weight:
+                        new_vertex.append(coordinate)
+                        all_new_edges.append(quasi_paths[coordinate])
+                        all_paths[coordinate][tuple(sorted(nodes_in_omega))] = quasi_paths[coordinate]
+
+                for node in pometka_lambda:
+                    if pometka_lambda[node] == new_min_weight:
+                        new_vertex.append(node)
+                        all_new_edges.append(pometka_lambda_path[node])
+                        all_paths[node][tuple(sorted(nodes_in_omega))] = pometka_lambda_path[node]
+                new_vertex = list(set(new_vertex))
+                new_edges = set()
+                for path in all_new_edges:
+                    new_edges.update(path)
+                new_edges = list(new_edges)
+
                 quasi_coordinates = {key: quasi_coordinates[key] for key in quasi_coordinates if
                                      key not in new_vertex}
+                quasi_paths = {key: quasi_paths[key] for key in quasi_paths if key not in new_vertex}
                 for vertex in new_vertex:
                     pometka_lambda.pop(vertex)
-                new_ro = Ro(new_vertex, new_min_weight)
+                    pometka_lambda_path.pop(vertex)
+                new_ro = Ro(new_vertex, new_edges, new_min_weight)
                 number_ro += 1
                 lot_of_pi[number_ro] = new_vertex
                 fi += [vertex for vertex in new_vertex if vertex not in fi]
@@ -237,8 +288,10 @@ def dijkstra4(G, init_nodes):
                 all_nodes = [node for node in all_nodes if node not in fi]
 
             if check_print_debug:
-                print("\npometka_lambda", pometka_lambda)
-                print("lot_of_ro", lot_of_ro)
+                print("lot_of_ro", end=' { ')
+                for key, value in lot_of_ro.items():
+                    print(f"{key}: {value}", end=" |||| ")
+                print("}")
                 # print("lot_of_pi", lot_of_pi)
                 # print("fi", fi, "\n")
             for ro in lot_of_ro.values():
@@ -249,34 +302,29 @@ def dijkstra4(G, init_nodes):
             # print("stein nodes:", stein_nodes)
 
     if check_print_debug:
+        print("\nall_coordinates")
         for key in all_coordinates:
             print(key, ": ", all_coordinates[key], sep="")
         print()
+        print("all_paths")
+        for key in all_paths:
+            print(key, ": ", all_paths[key], sep="")
+        print()
+
+    result_length = all_coordinates[init_nodes[len(init_nodes) - 1]][tuple(sorted(init_nodes[:len(init_nodes) - 1]))]
+    result_path = all_paths[init_nodes[len(init_nodes) - 1]][tuple(sorted(init_nodes[:len(init_nodes) - 1]))]
 
     print(
-        f"length: {all_coordinates[init_nodes[len(init_nodes) - 1]][tuple(sorted(init_nodes[:len(init_nodes) - 1]))]}")
+        f"length: {result_length}")
+    print(
+        f"path: {result_path}")
+
+    return result_path
 
 
 # main:
 G = nx.Graph()
 initialize_graph(G)
-
-# дейкстра для 2 и 3 точек
-# init_nodes = [1, 3, 9]
-# print("init nodes: ", init_nodes, "\n")
-#
-# if len(init_nodes) == 2:
-#     dijkstra2(G, init_nodes[0], init_nodes[1])
-# elif len(init_nodes) == 3:
-#     dijkstra3(G, init_nodes)
-#
-# print("\ncount operation: %d" % count_operation)
-
-# print("----------------------")
-# print("min path:", nx.single_source_dijkstra_path(G, 1))
-# print("min path length:", nx.single_source_dijkstra_path_length(G, 1))
-# print("----------------------")
-
 
 # дейкстра для 4 точек
 # init_nodes4 = [1, 3, 8, 9]  # норм
@@ -284,7 +332,7 @@ initialize_graph(G)
 # dijkstra4(G, init_nodes4)
 print()
 check_print_debug = True
-# dijkstra4(G, [1, 8, 6, 10])
-dijkstra4(G, [1, 10, 6, 8])
+result_path = dijkstra4(G, [1, 8, 6, 10])
+# dijkstra4(G, [1, 10, 6, 8])
 # dijkstra4(G, [1, 8, 6, 10, 3])
-print_graph(G)
+print_graph(G, result_path)
